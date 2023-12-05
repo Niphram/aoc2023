77afba5c9c260b900e6269237667ec3fd19111b8
Niphram <theniphram@gmail.com>
2023-12-05 22:33:14 +0100

    Day 5 Cleanup

--- src/day05.zig
+++ src/day05.zig
@@ -4,67 +4,6 @@ const util = @import("./util.zig");
 const Range = struct {
     const Self = @This();
 
-    source_start: usize,
-    dest_start: usize,
-    len: usize,
-
-    fn map(self: Self, val: usize) ?usize {
-        if (val >= self.source_start and val < (self.source_start + self.len)) {
-            return val + self.dest_start - self.source_start;
-        }
-
-        return null;
-    }
-
-    fn parse(input: []const u8) !Self {
-        var iter = std.mem.splitScalar(u8, input, ' ');
-        const dest_start = try std.fmt.parseInt(usize, iter.next().?, 10);
-        const source_start = try std.fmt.parseInt(usize, iter.next().?, 10);
-        const len = try std.fmt.parseInt(usize, iter.next().?, 10);
-
-        return Self{ .source_start = source_start, .dest_start = dest_start, .len = len };
-    }
-};
-
-fn part1(input: []const u8) !usize {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const seeds_s, const maps_s = util.splitSequenceOnce(u8, input, "\n\n");
-
-    var data = std.ArrayList(usize).init(allocator);
-    defer data.deinit();
-
-    var seed_iter = std.mem.splitScalar(u8, util.splitScalarOnce(u8, seeds_s, ' ')[1], ' ');
-    while (seed_iter.next()) |seed_s| {
-        const seed = try std.fmt.parseInt(usize, seed_s, 10);
-        try data.append(seed);
-    }
-
-    for (data.items) |*d| {
-        var iter = std.mem.tokenizeSequence(u8, maps_s, "\n\n");
-
-        while (iter.next()) |map_s| {
-            var map_iter = std.mem.tokenizeScalar(u8, map_s, '\n');
-            _ = map_iter.next();
-
-            while (map_iter.next()) |map| {
-                const range = try Range.parse(map);
-                if (range.map(d.*)) |new_val| {
-                    d.* = new_val;
-                    break;
-                }
-            }
-        }
-    }
-
-    return std.mem.min(usize, data.items);
-}
-
-const Range2 = struct {
-    const Self = @This();
-
     start: isize,
     len: isize,
 
@@ -73,132 +12,186 @@ const Range2 = struct {
         return self.start + self.len;
     }
 
-    fn split(self: Self, val: isize) std.meta.Tuple(&[_]type{ Self, Self }) {
-        const left = Self{ .start = self.start, .len = val - self.start };
-        const right = Self{ .start = val, .len = self.len - left.len };
+    // Splits the range into two parts (value will be contained in right half)
+    fn split(self: Self, value: isize) std.meta.Tuple(&[_]type{ Self, Self }) {
+        const left = Self{ .start = self.start, .len = value - self.start };
+        const right = Self{ .start = value, .len = self.len - left.len };
 
         return .{ left, right };
     }
 
-    fn intersect(self: Self, other: Self) bool {
-        if (self.start >= other.start and self.start < other.start + other.len) {
-            return true;
-        } else if (other.start >= self.start and other.start < self.start + self.len) {
-            return true;
-        }
+    // Returns true, if this range contains the value (inclusive)
+    fn containsValue(self: Self, value: isize) bool {
+        return value >= self.start and value < self.end();
+    }
 
-        return false;
+    // Returns true, if this range completely contains the other range (inclusive)
+    fn containsRange(self: Self, other: Self) bool {
+        return self.containsValue(other.start) and self.containsValue(other.end() - 1);
     }
 };
 
 const RangeMapper = struct {
     const Self = @This();
 
-    range: Range2,
+    range: Range,
     offset: isize,
 
     fn parse(input: []const u8) !RangeMapper {
         var iter = std.mem.splitScalar(u8, input, ' ');
-        const dest_start = try std.fmt.parseInt(isize, iter.next().?, 10);
-        const start = try std.fmt.parseInt(isize, iter.next().?, 10);
+        const dest = try std.fmt.parseInt(isize, iter.next().?, 10);
+        const source = try std.fmt.parseInt(isize, iter.next().?, 10);
         const len = try std.fmt.parseInt(isize, iter.next().?, 10);
-        const offset: isize = dest_start - start;
+        const offset: isize = dest - source;
 
         return Self{
-            .range = Range2{ .start = start, .len = len },
+            .range = Range{ .start = source, .len = len },
             .offset = offset,
         };
     }
+
+    // If the value is contained in the range, map it to it's new value
+    // Otherwise return null
+    fn map(self: Self, value: isize) ?isize {
+        if (self.range.containsValue(value)) {
+            return value + self.offset;
+        }
+
+        return null;
+    }
 };
+
+fn part1(input: []const u8) !isize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // Split input into seeds and category-maps
+    const seed_line, const maps_input = util.splitSequenceOnce(u8, input, "\n\n");
+
+    // List of locations to be mapped
+    var locations = std.ArrayList(isize).init(allocator);
+    defer locations.deinit();
+
+    // Read seeds
+    var seed_iter = std.mem.splitScalar(u8, util.splitScalarOnce(u8, seed_line, ' ')[1], ' ');
+    while (seed_iter.next()) |seed| {
+        try locations.append(try std.fmt.parseInt(isize, seed, 10));
+    }
+
+    // Iterate through each location
+    for (locations.items) |*location| {
+        var map_category_iter = std.mem.tokenizeSequence(u8, maps_input, "\n\n");
+
+        // Iterate through all category maps
+        while (map_category_iter.next()) |category| {
+            var map_iter = std.mem.tokenizeScalar(u8, category, '\n');
+
+            // Skip map header
+            _ = map_iter.next();
+
+            // Iterate through every map
+            while (map_iter.next()) |map| {
+                const mapper = try RangeMapper.parse(map);
+
+                // If location is in the range, map it
+                if (mapper.map(location.*)) |new_val| {
+                    location.* = new_val;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Return minimum
+    return std.mem.min(isize, locations.items);
+}
 
 fn part2(input: []const u8) !isize {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    // Split input into seeds and category-maps
     const seeds_s, const maps_s = util.splitSequenceOnce(u8, input, "\n\n");
 
-    var ranges = std.ArrayList(Range2).init(allocator);
+    // Keeps track of all seed ranges
+    var ranges = std.ArrayList(Range).init(allocator);
     defer ranges.deinit();
 
+    // Read all seed ranges
     var seed_iter = std.mem.splitScalar(u8, util.splitScalarOnce(u8, seeds_s, ' ')[1], ' ');
-    while (seed_iter.next()) |seed_s| {
-        const start = try std.fmt.parseInt(isize, seed_s, 10);
+    while (seed_iter.next()) |start_s| {
+        const start = try std.fmt.parseInt(isize, start_s, 10);
         const len = try std.fmt.parseInt(isize, seed_iter.next().?, 10);
 
-        try ranges.append(Range2{ .start = start, .len = len });
+        try ranges.append(Range{ .start = start, .len = len });
     }
-
-    var minimum: isize = std.math.maxInt(isize);
 
     // Map
     var iter = std.mem.tokenizeSequence(u8, maps_s, "\n\n");
     while (iter.next()) |map_s| {
-        const map_title, const map_data = util.splitScalarOnce(u8, map_s, '\n');
+        var map_iter = std.mem.tokenizeScalar(u8, map_s, '\n');
 
-        std.debug.print("{s}\n", .{map_title});
+        // Skip category header
+        _ = map_iter.next();
 
-        var new_ranges = std.ArrayList(Range2).init(allocator);
-        defer new_ranges.deinit();
+        // Keep track of already mapped ranges, skip those for the rest of the category
+        var completed_ranges: usize = 0;
 
-        var map_iter = std.mem.tokenizeScalar(u8, map_data, '\n');
         while (map_iter.next()) |map| {
             const mapper = try RangeMapper.parse(map);
 
-            std.debug.print("Mapper: {any}\n", .{mapper});
+            // Iterate through all unmapped ranges
+            // List Layout => [...completed_ranges][...ranges][...new_ranges]
+            for (completed_ranges..ranges.items.len) |range_idx| {
+                {
+                    // Get range from list
+                    const range = &ranges.items[range_idx];
 
-            // Ranges
-            const len = ranges.items.len;
-            for (0..len) |i| {
-                _ = i;
-
-                var range = ranges.orderedRemove(0);
-
-                std.debug.print("Range begin: {any}\n", .{range});
-
-                // Left
-                if (range.start < mapper.range.start and range.end() > mapper.range.start) {
-                    const left, const right = range.split(mapper.range.start);
-
-                    std.debug.print("Left: {any}\n", .{left});
-
-                    try ranges.append(left);
-                    range = right;
+                    // If range intersects the mapped range on the left side
+                    if (range.*.start < mapper.range.start and mapper.range.start < range.*.end()) {
+                        // Split the range
+                        const left, range.* = range.*.split(mapper.range.start);
+                        try ranges.append(left);
+                    }
                 }
 
-                // Right
-                if (range.start < mapper.range.end() and range.end() > mapper.range.end()) {
-                    const left, const right = range.split(mapper.range.end());
+                {
+                    // Get range from list, may have changed because of reallocation
+                    const range = &ranges.items[range_idx];
 
-                    std.debug.print("Right: {any}\n", .{right});
-
-                    try ranges.append(right);
-                    range = left;
+                    // If range intersects the mapped range on the right side
+                    if (range.*.start < mapper.range.end() and mapper.range.end() < range.*.end()) {
+                        // Split the range
+                        range.*, const right = range.*.split(mapper.range.end());
+                        try ranges.append(right);
+                    }
                 }
 
-                // Middle
-                if (range.start >= mapper.range.start and range.end() <= mapper.range.end()) {
-                    range.start += mapper.offset;
+                {
+                    // Get range from list, may have changed because of reallocation
+                    const range = &ranges.items[range_idx];
 
-                    std.debug.print("Middle: {any}\n", .{range});
-
-                    try new_ranges.append(range);
-                } else {
-                    std.debug.print("Range untouched\n", .{});
-                    try ranges.append(range);
+                    // If the range is completely contained inside
+                    if (mapper.range.containsRange(range.*)) {
+                        // Offset the range
+                        range.*.start += mapper.offset;
+                        // Move range to the completed part of the list
+                        std.mem.swap(Range, range, &ranges.items[completed_ranges]);
+                        // Increment to skip this range for the current category
+                        completed_ranges += 1;
+                    }
                 }
             }
-
-            std.debug.print("\n", .{});
         }
+    }
 
-        try ranges.appendSlice(new_ranges.items);
+    // Find lowest starting location
+    var minimum: isize = std.math.maxInt(isize);
 
-        if (iter.peek() == null) {
-            for (ranges.items) |range| {
-                minimum = @min(minimum, range.start);
-            }
-        }
+    for (ranges.items) |range| {
+        minimum = @min(minimum, range.start);
     }
 
     return minimum;
