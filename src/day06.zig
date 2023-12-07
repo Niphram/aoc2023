b89ab1a339783eccbd62d77f3a2ac11aef9fa086
Niphram <theniphram@gmail.com>
2023-12-07 21:31:09 +0100

    Day 6 rework

--- src/day06.zig
+++ src/day06.zig
@@ -1,65 +1,79 @@
 const std = @import("std");
 const util = @import("./util.zig");
 
-const Boat = struct { speed: usize = 0 };
+const Race = struct {
+    const Self = @This();
+
+    time: usize,
+    distance: usize,
+
+    // Calculate the number of possible hold times
+    fn marginOfError(self: Self) usize {
+        const time: f64 = @floatFromInt(self.time);
+        const dis: f64 = @floatFromInt(self.distance);
+
+        // Derived from 'dis = hold_time * (time - hold_time)'
+        const common: f64 = @sqrt(time * time - 4 * dis);
+        const min: usize = @as(usize, @intFromFloat(0.5 * (time - common)));
+        const max: usize = @as(usize, @intFromFloat(0.5 * (time + common)));
+
+        return max - min;
+    }
+};
 
 fn part1(input: []const u8) !usize {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-    _ = allocator;
+    var line_iter = std.mem.tokenizeScalar(u8, input, '\n');
 
-    // Split input into seeds and category-maps
-    const times_s, const distances_s = util.splitScalarOnce(u8, input, '\n');
+    // Get input lines
+    const time_line = line_iter.next().?;
+    const dis_line = line_iter.next().?;
 
-    var time_iter = std.mem.tokenizeScalar(u8, times_s[9..], ' ');
-    var distance_iter = std.mem.tokenizeScalar(u8, distances_s[9..], ' ');
+    var time_iter = std.mem.tokenizeScalar(u8, util.splitScalarOnce(u8, time_line, ':')[1], ' ');
+    var dis_iter = std.mem.tokenizeScalar(u8, util.splitScalarOnce(u8, dis_line, ':')[1], ' ');
 
-    var total: usize = 1;
+    var total_margin_of_error: usize = 1;
 
+    // Iterate through both lines at once
     while (time_iter.next()) |time_s| {
-        const dis_s = distance_iter.next().?;
+        const dis_s = dis_iter.next().?;
 
-        std.debug.print("{s}|{s}\n", .{ time_s, dis_s });
-
+        // Parse time and distance
         const time = try std.fmt.parseInt(usize, time_s, 10);
         const dis = try std.fmt.parseInt(usize, dis_s, 10);
 
-        var possible_times: usize = 0;
-
-        for (0..time + 1) |hold_time| {
-            const rem_time = time - hold_time;
-            const travelled_dis = rem_time * hold_time;
-
-            if (travelled_dis > dis) {
-                possible_times += 1;
-            }
-        }
-
-        total *= possible_times;
+        // Calculate margin of error
+        const race = Race{ .time = time, .distance = dis };
+        total_margin_of_error *= race.marginOfError();
     }
 
-    return total;
+    return total_margin_of_error;
+}
+
+// Read buffer and build number from all digits.
+// Ignores any non-digits
+fn readAllDigits(input: []const u8) usize {
+    var number: usize = 0;
+
+    for (input) |c| {
+        if (std.ascii.isDigit(c)) {
+            number = number * 10 + (c - '0');
+        }
+    }
+
+    return number;
 }
 
 fn part2(input: []const u8) !usize {
-    _ = input;
+    var line_iter = std.mem.tokenizeScalar(u8, input, '\n');
 
-    const time = 46828479;
-    const dis = 347152214061471;
+    const time_line = line_iter.next().?;
+    const dis_line = line_iter.next().?;
 
-    var possible_times: usize = 0;
+    const time = readAllDigits(util.splitScalarOnce(u8, time_line, ':')[1]);
+    const dis = readAllDigits(util.splitScalarOnce(u8, dis_line, ':')[1]);
 
-    for (0..time + 1) |hold_time| {
-        const rem_time = time - hold_time;
-        const travelled_dis = rem_time * hold_time;
-
-        if (travelled_dis > dis) {
-            possible_times += 1;
-        }
-    }
-
-    return possible_times;
+    const race = Race{ .time = time, .distance = dis };
+    return race.marginOfError();
 }
 
 pub fn main() !void {
